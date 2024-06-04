55624830d5f2e3b357ca52ee62ede3585162ea2d
Dugon <168607663+Eegpv@users.noreply.github.com>
2024-06-04 23:49:21 +0300

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: MySQL-5.7
+-- Время создания: Июн 04 2024 г., 23:48
+-- Версия сервера: 5.7.44
+-- Версия PHP: 8.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `MyfDB`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `email` varchar(319) NOT NULL,
+  `age` tinyint(4) NOT NULL,
+  `password` binary(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `age`, `password`) VALUES
+(1, 'Ivan', 'test@gmail.com', 33, 0x300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000),
+(2, 'Elena', 'test11@gmail.com', 22, 0x300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000),
+(3, 'Anton', 'test33@gmail.com', 44, 0x300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
